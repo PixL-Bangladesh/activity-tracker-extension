f4b4eafaf183e69cdb148c7cae95fca3617ab44a
Abrar Mahir Esam <abrarme118@gmail.com>
2025-06-02 13:02:07 +0600

    #update-api-url

--- v2/src/config.ts
+++ v2/src/config.ts
@@ -2,6 +2,6 @@ const HOSTED_URL = "https://activity-tracker-extension-three.vercel.app";
 const DEVELOPMENT_URL = "http://localhost:3000";
 
 export const config = {
-  SERVER_URL: `${DEVELOPMENT_URL}/api`,
-  SITE_URL: DEVELOPMENT_URL,
+  SERVER_URL: `${HOSTED_URL}/api`,
+  SITE_URL: HOSTED_URL,
 };
